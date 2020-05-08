ab47bad105dca3fd58c2a18025bc349bae72dc17
Elena Laakso <elena.laakso@metropolia.fi>
2020-05-08 14:14:54 +0300

    modle export

--- remote.js
+++ remote.js
@@ -4,17 +4,18 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 
+app.enable('trust proxy');
+
+app.use ((req, res, next) => {
+    if (req.secure) {
+        next();
+    } else {
+        const proxypath = process.env.PROXY_PASS || ''
+        res.redirect(301, `https://${req.headers.host}${proxypath}${req.url}`);
+    }
+});
 module.exports = (app, httpPort) => {
-    app.enable('trust proxy');
-
-    app.use ((req, res, next) => {
-        if (req.secure) {
-            next();
-        } else {
-            const proxypath = process.env.PROXY_PASS || ''
-            res.redirect(301, `https://${req.headers.host}${proxypath}${req.url}`);
-        }
-    });
-
     app.listen(httpPort);
 };
+
+
